c41f7b83f253406a604468249b4d6b4f22377e5c
Isaac <git@eartharoid.me>
2022-06-12 23:11:52 +0100

    static

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter() },
+	kit: { adapter: adapter({ fallback: 'index.html' }) },
 	preprocess: [
 		preprocess({ postcss: true })
 	]
